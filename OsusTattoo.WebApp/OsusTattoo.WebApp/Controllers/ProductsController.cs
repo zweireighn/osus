c21aeb7cd6167740098bba9c50f3456f68e39a7f
DESKTOP-V484ETJ\Angelo <angelojames.rosales@gmail.com>
2025-05-19 23:08:49 +0800

    Other pages changes

---
+++ OsusTattoo.WebApp/OsusTattoo.WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OsusTattoo.WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult AllProducts()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
