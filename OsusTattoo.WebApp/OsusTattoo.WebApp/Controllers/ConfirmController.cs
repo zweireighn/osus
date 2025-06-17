ddfdc45755b73ad916ba48202144c0b0e110def2
DESKTOP-V484ETJ\Angelo <angelojames.rosales@gmail.com>
2025-06-17 21:43:38 +0800

    Confirm page

---
+++ OsusTattoo.WebApp/OsusTattoo.WebApp/Controllers/ConfirmController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OsusTattoo.WebApp.Controllers
+{
+    public class ConfirmController : Controller
+    {
+        // GET: Confirm
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
